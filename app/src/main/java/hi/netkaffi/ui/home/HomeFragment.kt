468361374317c02546b47b1d4fe0ba5a31e68da1
RbrtNkvi <robert_nokkvi@hotmail.com>
2024-02-21 18:50:18 +0000

    klasar

--- app/src/main/java/hi/netkaffi/ui/home/HomeFragment.kt
+++ app/src/main/java/hi/netkaffi/ui/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package hi.netkaffi.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import hi.netkaffi.activities.HomeActivity
+import hi.netkaffi.activities.MainActivity
 import hi.netkaffi.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,10 +33,14 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val listView: ListView = binding.products
+        val arrayAdapter: ArrayAdapter<*>
+        val products = arrayOf("computer1","computer2","laptop")
+        val context = context as MainActivity
+        arrayAdapter = ArrayAdapter(
+            context,
+            android.R.layout.simple_list_item_1 ,products)
+        listView.adapter = arrayAdapter
         return root
     }
 
