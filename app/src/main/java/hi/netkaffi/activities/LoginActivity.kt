02680620b1d06412b5e344c2e859f2efc506a19f
ves14 <54890610+ves14@users.noreply.github.com>
2024-03-03 22:51:54 +0000

    Activity to login/signuo
    Added dumy activity to login and sign up. Resets after each session
    
    User :
      username: 123
      password: 123
    starts as active

--- app/src/main/java/hi/netkaffi/activities/LoginActivity.kt
+++ app/src/main/java/hi/netkaffi/activities/LoginActivity.kt
@@ -3,10 +3,13 @@ package hi.netkaffi.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import hi.netkaffi.R
+import hi.netkaffi.entities.User
+import hi.netkaffi.service.dummyData
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,17 +24,25 @@ class LoginActivity : AppCompatActivity() {
             val intentSignup = Intent(this, SignupActivity::class.java)
             startActivity(intentSignup)
         }
+        val tempuser = User("123","123")
+        dummyData.Users.addUsers(tempuser)
 
         val buttonMain = findViewById<Button>(R.id.login_button);
         buttonMain.setOnClickListener{
 
-            val  username = findViewById<EditText>(R.id.login_username)
-            //Toast.makeText(this,username.text,Toast.LENGTH_LONG).show()
-            val password = findViewById<EditText>(R.id.login_password)
-            //Toast.makeText(this,password.text,Toast.LENGTH_LONG).show()
+            val  username = findViewById<EditText>(R.id.login_username).getText().toString()
+            val password = findViewById<EditText>(R.id.login_password).getText().toString()
 
-            val intentMain = Intent(this, MainActivity::class.java)
-            startActivity(intentMain)
+
+            val user = User(username,password)
+            if(dummyData.Users.isUser(user)){
+                val intentMain = Intent(this, MainActivity::class.java)
+                startActivity(intentMain)
+            }
+            else
+            Toast.makeText(this,"Wrong Username or Password",Toast.LENGTH_LONG).show()
+
+
         }
 
 
