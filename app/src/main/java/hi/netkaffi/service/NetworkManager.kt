15050d7ab15d5bfddf1d55f4f501ed109fe612dd
ves14 <54890610+ves14@users.noreply.github.com>
2024-03-07 18:16:20 +0000

    Update NetworkManager.kt

--- app/src/main/java/hi/netkaffi/service/NetworkManager.kt
+++ app/src/main/java/hi/netkaffi/service/NetworkManager.kt
@@ -2,4 +2,11 @@ package hi.netkaffi.service
 
 class NetworkManager {
 
-}
+    fun doPOST(address: String){
+        val serverAddress = "https://" + address + ""
+    }
+
+    fun doGET(address: String){
+        val serverAddress = "https://" + address + ""
+    }
+} 
